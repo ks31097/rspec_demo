e9894727b693fb9b1918ef8eaf439a51314578ec
Kostya Sigarov <ksigaryov@gmail.com>
2022-07-19 22:50:52 +0300

    Add test for car

--- car.rb
+++ car.rb
@@ -18,7 +18,7 @@ class Car
   end
 end
 
-car = Car.new
-car.add_fuel 10
+# car = Car.new
+# car.add_fuel 10
 
-puts "Range is #{car.range}" # сможем проехать
+# puts "Range is #{car.range}" # сможем проехать
